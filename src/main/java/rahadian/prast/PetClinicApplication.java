f3c82bc6fc21b46b963726d16620da1d744bdf78
v4llcrist <rahadian.prast@gmail.com>
2024-12-28 15:38:17 +0700

    first setup

---
+++ src/main/java/rahadian/prast/PetClinicApplication.java
@@ -0,0 +1,16 @@
+package rahadian.prast;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @Author ian
+ * @create 28/12/24 15.34
+ */
+@SpringBootApplication
+public class PetClinicApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PetClinicApplication.class, args);
+    }
+}
